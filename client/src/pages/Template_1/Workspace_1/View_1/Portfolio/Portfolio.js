5c52da7bcb1579fdd007a1fff39ad386f19d0ebe
Binh Lam <cindybinh444@gmail.com>
2019-01-08 18:05:01 -0500

    add template1 to view_1

--- client/src/pages/Template_1/Workspace_1/View_1/Portfolio/Portfolio.js
+++ client/src/pages/Template_1/Workspace_1/View_1/Portfolio/Portfolio.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import "./Portfolio.css";
-import one from '../../../assets/images/one.png'; // with import
-import two from '../../../assets/images/two.png';
-import three from '../../../assets/images/three.png';
-import four from '../../../assets/images/four.png';
-import five from '../../../assets/images/five.png';
-import six from '../../../assets/images/six.png';
+import one from '../assets/images/one.png'; // with import
+import two from '../assets/images/two.png';
+import three from '../assets/images/three.png';
+import four from '../assets/images/four.png';
+import five from '../assets/images/five.png';
+import six from '../assets/images/six.png';
 class About extends Component {
 
     render() {
--- client/src/pages/Template_1/Workspace_1/View_1/View_1.js
+++ client/src/pages/Template_1/Workspace_1/View_1/View_1.js
@@ -1,5 +1,11 @@
 import React, { Component } from "react";
 import Nav from "../../../../components/Nav";
+import Temp13D from "./temp13D/Temp13D";
+import Temp1Nav from "./temp1Nav";
+import About from "./About";
+import Portfolio from "./Portfolio";
+import Contact from "./Contact";
+import Temp1Foot from "./temp1foot";
 
 class View_1 extends Component {
   state = {};
@@ -13,6 +19,23 @@ class View_1 extends Component {
         />
         <div className="jumbotron mt-5 text-center">
           This is where you VIEW contents!!!
+          <div>
+                <Temp1Nav />
+                <Temp13D />
+                <div div className="abt" style={{ display: 'flex', justifyContent: 'center', }}>
+                    <About />
+                </div>
+                <div div className="port" style={{ display: 'flex', justifyContent: 'center', }}>
+                    <Portfolio />
+                </div>
+                <div div className="port" style={{ display: 'flex', justifyContent: 'left', }}>
+                    <Contact />
+                </div>
+                <div div className="port" style={{ display: 'flex', justifyContent: 'left', }}>
+                    <Temp1Foot />
+                </div>
+            </div>
+
         </div>
       </div>
     );
