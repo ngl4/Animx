0ade9662935d3dc6340064dd1c7ac7a81bafef8f
Binh Lam <cindybinh444@gmail.com>
2019-01-17 21:20:23 -0500

    add mongodb uri

---
+++ client/build/precache-manifest.ddd348996034dc9ce40d5f320b7f519b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c7bd043c56f8be14fa6",
+    "url": "/static/css/main.450d4ec6.chunk.css"
+  },
+  {
+    "revision": "2c7bd043c56f8be14fa6",
+    "url": "/static/js/main.2c7bd043.chunk.js"
+  },
+  {
+    "revision": "1ff967a080cc66f17bd6",
+    "url": "/static/js/1.1ff967a0.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ae159f2cc094f59d7e041b3dda308faf",
+    "url": "/static/media/bldg.ae159f2c.jpg"
+  },
+  {
+    "revision": "92f45d47e85dd021e7f00724666df916",
+    "url": "/static/media/one.92f45d47.jpg"
+  },
+  {
+    "revision": "225a1c038688bfa1e4811c32dddcedcf",
+    "url": "/static/media/two.225a1c03.jpg"
+  },
+  {
+    "revision": "9fab5547d9d94ee28f60f752d16dc1c8",
+    "url": "/static/media/three.9fab5547.jpg"
+  },
+  {
+    "revision": "7e5d9d0556dd62c1db785b29879d4cd8",
+    "url": "/static/media/four.7e5d9d05.jpg"
+  },
+  {
+    "revision": "2b9fbb7d24773268f203b89f0ad49e88",
+    "url": "/static/media/five.2b9fbb7d.jpg"
+  },
+  {
+    "revision": "d80d9399463a184ecc7c7b29bec39c1e",
+    "url": "/static/media/six.d80d9399.jpg"
+  },
+  {
+    "revision": "b5f4f58c9f45d4c747957a2aeef42eb8",
+    "url": "/static/media/bg-triangle.b5f4f58c.jpg"
+  },
+  {
+    "revision": "ea7e385f34ad7fdc06af62f7c0bbc48b",
+    "url": "/index.html"
+  }
+];
